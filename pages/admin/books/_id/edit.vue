958d0e9c7fbfa056bfbb243fd3c937216714d86d
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-05-08 14:25:04 +0400

    feat(operator): start edit page

---
+++ pages/admin/books/_id/edit.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="operator-container">
+    <div class="description">
+      <h1 class="title">Book title goes here</h1>
+      <h2 class="author">Author goes here</h2>
+    </div>
+    <div class="img-wrapper">
+      <img
+        id="main-img"
+        ref="mainImg"
+        src="~/assets/images/test.jpg"
+        alt="Scanned Page"
+      />
+      <div class="canvas"></div>
+    </div>
+    <div class="bottom-panel">
+      <div class="letter-input">
+        <input type="text" />
+        <button>დადასტურება</button>
+      </div>
+      <button>მართკუთხედის დამატება</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Operator',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.operator-container {
+  .description {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 15vh;
+
+    .title {
+      font-size: 2rem;
+    }
+
+    .author {
+      font-size: 1rem;
+    }
+  }
+
+  .img-wrapper {
+    border: 1px solid black;
+    overflow: scroll;
+    position: relative;
+    height: 65vh;
+
+    img {
+      width: 100%;
+    }
+
+    .canvas {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
+
+  .bottom-panel {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 40px;
+    gap: 20px;
+
+    .letter-input {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+      gap: 10px;
+    }
+  }
+}
+</style>
--- plugins/draggable-resizable.js
+++ plugins/draggable-resizable.js
@@ -2,4 +2,4 @@ import Vue from 'vue'
 import VueDraggableResizable from 'vue-draggable-resizable'
 import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
 
-Vue.component('vue-draggable-resizable', VueDraggableResizable)
+Vue.component('VueDraggableResizable', VueDraggableResizable)
