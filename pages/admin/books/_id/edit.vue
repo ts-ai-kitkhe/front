846317c111c50ebaa84e05a9338ea425dc30d23e
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-05-10 16:17:31 +0400

    feat(operator): fetch and render boxes from json

--- pages/admin/books/_id/edit.vue
+++ pages/admin/books/_id/edit.vue
@@ -10,30 +10,101 @@
         ref="mainImg"
         src="~/assets/images/test.jpg"
         alt="Scanned Page"
+        @load="fetchData"
       />
-      <div class="canvas"></div>
+      <div class="canvas">
+        <div v-for="(rect, index) in rectCoords" :key="index">
+          <VueDraggableResizable
+            class-name-active="rect-active"
+            :w="rect.w"
+            :h="rect.h"
+            :x="rect.x"
+            :y="rect.y"
+            @activated="onActivated"
+            @deactivated="onDeactivated"
+          >
+            <div class="letter">
+              <p>{{ rect.char }}</p>
+            </div>
+          </VueDraggableResizable>
+        </div>
+      </div>
     </div>
     <div class="bottom-panel">
-      <div class="letter-input">
+      <div v-show="!addRectMode" class="letter-input">
         <input type="text" />
         <button>დადასტურება</button>
       </div>
-      <button>მართკუთხედის დამატება</button>
+      <button v-show="addRectMode">მართკუთხედის დამატება</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      rectCoords: [],
+      activatedCounter: 0,
+    }
+  },
+
   head() {
     return {
       title: 'Operator',
     }
   },
+
+  computed: {
+    imageHeight() {
+      return this.$refs.mainImg.clientHeight
+    },
+
+    imageWidth() {
+      return this.$refs.mainImg.clientWidth
+    },
+
+    addRectMode() {
+      return this.activatedCounter === 0
+    },
+  },
+
+  methods: {
+    async fetchData() {
+      return await fetch('/coords.json')
+        .then((response) => response.json())
+        .then((data) => {
+          const imgShape = { h: 748, w: 570 }
+          const screenSize = { h: this.imageHeight, w: this.imageWidth }
+
+          const rescaleH = (x) => (x / imgShape.h) * screenSize.h
+          const rescaleW = (x) => (x / imgShape.w) * screenSize.w
+
+          this.rectCoords = Object.entries(data).map(([key, coords]) => ({
+            id: key,
+            x: rescaleW(coords[0][0]),
+            y: rescaleH(coords[0][1]),
+            h: rescaleH(coords[2][1] - coords[0][1]),
+            w: rescaleW(coords[2][0] - coords[0][0]),
+            char: 'ჯ',
+          }))
+        })
+    },
+
+    onActivated() {
+      this.activatedCounter++
+    },
+
+    onDeactivated() {
+      this.activatedCounter--
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+$handle-size: 7px;
+
 .operator-container {
   .description {
     display: flex;
@@ -66,6 +137,76 @@ export default {
       position: absolute;
       top: 0;
       left: 0;
+
+      .vdr {
+        outline: 1px solid black;
+        border: none;
+
+        .handle {
+          width: $handle-size;
+          height: $handle-size;
+          z-index: 111;
+        }
+
+        .handle-tl {
+          top: -$handle-size;
+          left: -$handle-size;
+        }
+
+        .handle-tm {
+          top: -$handle-size;
+          margin-left: calc($handle-size / -2);
+        }
+
+        .handle-tr {
+          top: -$handle-size;
+          right: -$handle-size;
+        }
+
+        .handle-ml {
+          left: -$handle-size;
+          margin-top: calc($handle-size / -2);
+        }
+
+        .handle-mr {
+          right: -$handle-size;
+          margin-top: calc($handle-size / -2);
+        }
+
+        .handle-bl {
+          bottom: -$handle-size;
+          left: -$handle-size;
+        }
+
+        .handle-bm {
+          bottom: -$handle-size;
+          margin-left: calc($handle-size / -2);
+        }
+
+        .handle-br {
+          bottom: -$handle-size;
+          right: -$handle-size;
+        }
+
+        .letter {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 100%;
+
+          p {
+            color: transparent;
+            cursor: default;
+            margin: 0;
+            text-align: center;
+          }
+        }
+
+        &.rect-active {
+          outline: 2px solid #a2cd48;
+          box-shadow: 0px 0px 0px 999999px rgba(0, 0, 0, 0.8);
+        }
+      }
     }
   }
 
