ae8eb9069e5982321571be06cf16a47ed953e3eb
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-06-06 03:03:33 +0400

    feat(book-card): add initial markup

---
+++ components/book-card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="book-card-container">
+    <div class="card">
+      <img
+        src="https://edit.org/images/cat/book-covers-big-2019101610.jpg"
+        alt="Book Card Image"
+      />
+      <div class="card-body">
+        <h5 class="book-title">Book title</h5>
+        <h6 class="book-author">Book author</h6>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.book-card-container {
+  .card {
+    width: 10rem;
+  }
+}
+</style>
