833d4c7b1bf8e0c4e21af40da438fc33f4985a21
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-06-09 14:49:00 +0400

    feat(book-card): add initial markup

---
+++ components/book-card.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="col-md-3">
+    <NuxtLink :to="`/books/${bookInfo.id}`" class="book-card-container">
+      <div class="card">
+        <img :src="bookInfo.image" alt="Book Card Image" />
+        <div class="card-body">
+          <h5>{{ bookInfo.title }}</h5>
+          <h6>{{ bookInfo.author }}</h6>
+        </div>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    bookInfo: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.book-card-container {
+  color: black;
+  text-decoration: none;
+
+  .card {
+    border: none;
+
+    .card-body {
+      text-align: center;
+
+      h5 {
+        font-size: 18px;
+      }
+
+      h6 {
+        font-size: 16px;
+        font-weight: 200;
+      }
+    }
+  }
+}
+</style>
