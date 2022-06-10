66a79bf885c1fc23e151d416d5806fb63e4a2a0b
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-06-10 23:41:04 +0400

    feat(books): add initial markup for filter bar

---
+++ components/filter-bar.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="sidebar-container pb-4">
+    <b-navbar toggleable="lg p-0">
+      <b-navbar-toggle target="nav-collapse">
+        <template #default>Filters</template>
+      </b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <div class="sidebar-dropdown my-3">
+          <p>Dropdown:</p>
+          <b-dd text="Choice 1">
+            <b-dd-item>Choice 1</b-dd-item>
+            <b-dd-item>Choice 2</b-dd-item>
+            <b-dd-item>Choice 3</b-dd-item>
+          </b-dd>
+        </div>
+        <div class="sidebar-form-group my-5">
+          <b-form-group label="Form Group">
+            <b-check-group :options="options" stacked></b-check-group>
+          </b-form-group>
+        </div>
+        <div class="sidebar-range my-5">
+          <label for="range">Range with step</label>
+          <b-input
+            v-model="rangeValue"
+            type="range"
+            min="0"
+            max="5"
+            step="0.5"
+          ></b-input>
+          <div>Value: {{ rangeValue }}</div>
+        </div>
+        <div class="sidebar-radio my-5">
+          <b-form-group label="Radio buttons">
+            <b-form-radio v-model="radio" value="A">Option A</b-form-radio>
+            <b-form-radio v-model="radio" value="B">Option B</b-form-radio>
+          </b-form-group>
+          <div class="mt-3">
+            Selected: <strong>{{ radio }}</strong>
+          </div>
+        </div>
+        <div class="sidebar-tags">
+          <label for="tags-basic">Tags</label>
+          <b-form-tags v-model="tags"></b-form-tags>
+          <p class="mt-2">tags: {{ tags }}</p>
+        </div>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: [
+        { text: 'Choice 1', value: 'Choice 1' },
+        { text: 'Choice 2', value: 'Choice 2' },
+        { text: 'Choice 3', value: 'Choice 3' },
+        { text: 'Choice 4', value: 'Choice 4' },
+      ],
+      radio: '',
+      rangeValue: '2',
+      tags: ['tag1', 'tag2'],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.sidebar-container {
+  .navbar {
+    justify-content: center;
+
+    .navbar-toggler {
+      color: black;
+      border: 1px solid black;
+    }
+
+    .navbar-collapse {
+      flex-direction: column;
+      align-items: flex-start;
+
+      .sidebar-dropdown {
+        .dropdown {
+          button {
+            display: flex;
+            align-items: center;
+            background-color: white;
+            color: black;
+          }
+
+          .dropdown-toggle::after {
+            margin-left: 20px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/books/index.vue
+++ pages/books/index.vue
@@ -2,14 +2,18 @@
   <div class="books-container content-wrap">
     <div class="container my-5">
       <div class="row">
-        <div class="col-lg-3">Sidebar</div>
-        <div class="col-lg-9">
-          <div class="row books-row">
-            <book-card
-              v-for="book in allBooks"
-              :key="book.id"
-              :book-info="book"
-            />
+        <div class="col-lg-3 px-5">
+          <filter-bar />
+        </div>
+        <div class="col-lg-9 px-lg-5">
+          <div class="books-row">
+            <div class="row">
+              <book-card
+                v-for="book in allBooks"
+                :key="book.id"
+                :book-info="book"
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -38,10 +42,12 @@ export default {
 <style lang="scss" scoped>
 .books-container {
   .books-row {
-    align-items: center;
+    .row {
+      align-items: center;
 
-    @include xs {
-      justify-content: center;
+      @include xs {
+        justify-content: center;
+      }
     }
   }
 }
