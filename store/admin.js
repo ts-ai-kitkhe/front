2f9d7838370e526b9a8e9ecbbbeb5640866b5a86
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-06-15 15:11:55 +0400

    feat(store): add Vuex mappers for admin panel

---
+++ store/admin.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+export const state = () => ({
+  adminBooks: [],
+  adminBook: null,
+})
+
+export const mutations = {
+  setAdminBooks: (state, adminBooks) => (state.adminBooks = adminBooks),
+  setAdminBook: (state, adminBook) => (state.adminBook = adminBook),
+}
+
+export const actions = {
+  async getAdminBooks({ commit }) {
+    const response = await axios.get('/books.json')
+    commit('setAdminBooks', response.data)
+  },
+
+  async getAdminBookById({ commit }, id) {
+    const response = await axios.get('/books.json')
+    commit('setAdminBook', response.data[id - 1])
+  },
+}
+
+export const getters = {
+  allAdminBooks: (state) => state.adminBooks,
+  adminBookById: (state) => state.adminBook,
+}
