b262bfdaa419eba0c4b6f2813ad9946b11ef7a3d
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-06-15 15:11:40 +0400

    feat(admin): add initial books catalogue page

---
+++ pages/admin/books/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="admin-books-container">
+    <div class="container">
+      <h1 class="my-5">ჩემი წიგნები</h1>
+      <div class="row">
+        <admin-book-card
+          v-for="book in allAdminBooks"
+          :key="book.id"
+          :admin-book="book"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  computed: mapGetters('admin', ['allAdminBooks']),
+
+  created() {
+    this.getAdminBooks()
+  },
+
+  methods: {
+    ...mapActions('admin', ['getAdminBooks']),
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.admin-books-container {
+  h1 {
+    text-align: center;
+  }
+
+  .row {
+    align-items: center;
+  }
+}
+</style>
