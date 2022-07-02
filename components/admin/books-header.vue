75a60f2900002ae9e971e4224150ee4fb2282dcb
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-07-02 18:38:14 +0400

    refactor(admin): replace upload route with modal

--- components/admin/books-header.vue
+++ components/admin/books-header.vue
@@ -5,7 +5,34 @@
       <b-navbar-toggle target="header-collapse"></b-navbar-toggle>
       <b-collapse id="header-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <button>წიგნის ატვირთვა</button>
+          <div class="upload-btn">
+            <button v-b-modal.upload-modal>წიგნის ატვირთვა</button>
+            <b-modal id="upload-modal" centered title="წიგნის ატვირთვა">
+              <form>
+                <b-form-group label="სათაური" label-for="title-input">
+                  <b-form-input
+                    id="title-input"
+                    v-model="title"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+                <b-form-group label="ავტორი" label-for="author-input">
+                  <b-form-input
+                    id="author-input"
+                    v-model="author"
+                    required
+                  ></b-form-input>
+                </b-form-group>
+                <b-form-group label="მთავარი სურათი" label-for="cover-image">
+                  <b-form-file
+                    id="cover-image"
+                    v-model="coverImage"
+                    required
+                  ></b-form-file>
+                </b-form-group>
+              </form>
+            </b-modal>
+          </div>
           <button>lorem@ipsum.com</button>
         </b-navbar-nav>
       </b-collapse>
