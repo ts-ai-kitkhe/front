777ac8fc362d428cdb20451abf62143d199aa376
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-08-16 14:26:12 +0400

    refactor(books): modify filter types

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -13,7 +13,52 @@
       </div>
       <div class="row">
         <div class="col-lg-3 px-lg-4">
-          <FilterBar />
+          <div class="sidebar-container pb-4">
+            <b-navbar toggleable="lg p-0">
+              <b-navbar-toggle target="nav-collapse">
+                <template #default>ფილტრები</template>
+              </b-navbar-toggle>
+              <b-collapse id="nav-collapse" is-nav>
+                <div class="sidebar-radio mt-1 mb-5">
+                  <b-form-group>
+                    <label class="filter-label sort-label mb-3">დასორტვა</label>
+                    <b-form-radio v-model="radio" value="new"
+                      >ბოლოს დამატებული</b-form-radio
+                    >
+                    <b-form-radio v-model="radio" value="titleabc"
+                      >სათაური (ანბანის მიხედვით)</b-form-radio
+                    >
+                    <b-form-radio v-model="radio" value="authorabc"
+                      >ავტორი (ანბანის მიხედვით)</b-form-radio
+                    >
+                    <b-form-radio v-model="radio" value="conf1"
+                      >დამაჯერებლობა (ზრდადობით)</b-form-radio
+                    >
+                    <b-form-radio v-model="radio" value="conf2"
+                      >დამაჯერებლობა (კლებადობით)</b-form-radio
+                    >
+                    <b-form-radio v-model="radio" value="year1"
+                      >გამოცემის თარიღი (ძველიდან ახლისკენ)</b-form-radio
+                    >
+                    <b-form-radio v-model="radio" value="year2"
+                      >გამოცემის თარიღი (ახლიდან ძველისკენ)</b-form-radio
+                    >
+                  </b-form-group>
+                </div>
+                <div class="sidebar-range">
+                  <label for="mr-slider" class="filter-label slider-label">
+                    <div>დამაჯერებლობის</div>
+                    <div>ინტერვალი</div>
+                  </label>
+                  <VueSlider
+                    id="mr-slider"
+                    v-model="value"
+                    :tooltip-formatter="'{value}%'"
+                  />
+                </div>
+              </b-collapse>
+            </b-navbar>
+          </div>
         </div>
         <div class="col-lg-9 px-lg-5">
           <div class="search-bar-container">
@@ -42,6 +87,7 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import 'vue-slider-component/theme/antd.css'
 
 export default {
   layout: 'catalogue',
@@ -49,6 +95,8 @@ export default {
   data() {
     return {
       search: '',
+      radio: '',
+      value: [13, 31],
     }
   },
 
@@ -76,6 +124,79 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.sidebar-container {
+  .navbar {
+    justify-content: center;
+    font-family: 'BPG Nino Mkhedruli';
+    font-size: 18px;
+
+    .navbar-toggler {
+      color: white;
+      border: 2px solid white;
+      padding: 10px 15px;
+      border-radius: 20px;
+      width: 100%;
+      transition: 0.1s ease-out;
+
+      @media screen and (max-width: 400px) {
+        width: 80%;
+      }
+
+      &:hover {
+        color: black;
+        background-color: white;
+      }
+    }
+
+    .navbar-collapse {
+      flex-direction: column;
+      align-items: flex-start;
+
+      @include md {
+        margin-top: 1rem;
+        padding: 1rem;
+      }
+
+      .filter-label {
+        font-size: 24px;
+        font-family: 'BPG Nino Mkhedruli Bold';
+      }
+
+      .sidebar-radio {
+        width: 100%;
+
+        .form-group {
+          text-align: center !important;
+
+          .custom-radio {
+            text-align: left !important;
+            margin: 10px 0;
+          }
+        }
+
+        .sort-label {
+          text-align: center;
+        }
+      }
+
+      .sidebar-range {
+        width: 100%;
+
+        .slider-label {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-direction: column;
+        }
+      }
+    }
+  }
+}
+
+.vue-slider {
+  margin-top: 30px;
+}
+
 .books-container {
   .books-cover-row {
     margin-bottom: 3rem;
