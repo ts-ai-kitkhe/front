846cd4e0c8f3ede4b0d0770951607bb2919a4d46
tamar-mepe <tdavi18@freeuni.edu.ge>
2022-08-18 17:28:28 +0400

    refactor: change favicon and title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'front',
+    title: 'ts-ai-kitkhe',
     htmlAttrs: {
       lang: 'en',
     },
